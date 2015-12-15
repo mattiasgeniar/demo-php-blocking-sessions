c48a97d2b56062f20fb1ddaccb1c3546d551d0d2
Mattias Geniar <m@ttias.be>
2015-12-15 21:28:19 +0100

    first commit

---
+++ ajax_blocking_request.php
@@ -0,0 +1,19 @@
+<?php
+
+// Open the session
+session_name('blocking_sessions');
+session_start();
+
+$counter = (int) $_GET['div'];
+
+// Read all the data from the session and close it
+$username = $_SESSION['username'];
+
+// Do all the application logic: call 3rd party APIs, do queries,
+// run all kind of wrong ORM-for-loops, ...
+sleep(4);
+
+// And done!
+// No need to explicitly close the session, that's done automatically.
+echo 'AJAX '. $counter;
+?>
---
+++ ajax_nonblocking_request.php
@@ -0,0 +1,22 @@
+<?php
+
+// Open the session
+session_name('nonblocking_sessions');
+session_start();
+$counter = (int) $_GET['div'];
+
+// Read all the data from the session and close it
+$username = $_SESSION['username'];
+
+// Release the session lock: behold, fast ajax calls!
+// (just don't try to write to the session after this, changes will be lost)
+session_write_close();
+
+// Do all the application logic: call 3rd party APIs, do queries,
+// run all kind of wrong ORM-for-loops, ...
+sleep(4);
+
+// And done!
+// No need to explicitly close the session, that's done automatically.
+echo 'AJAX '. $counter;
+?>
